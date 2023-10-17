065dc1b9e2e64eeba1a4fec200050fa6a6b71825
MNxMunno <bilolhuzayfa@gmail.com>
2023-10-17 10:39:31 +0500

    change

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ let modeBtn = document.getElementById("dark-light");
 modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
+
+const header = document.querySelector("header");
+const menuBtn = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("open");
+});
